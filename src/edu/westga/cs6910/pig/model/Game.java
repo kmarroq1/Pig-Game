fbcf6244f8ba6d113d6511522f70d7fd989efa7f
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-08 23:24:22 -0400

    Changed goal score back to 20 (originally changed for testing)

--- src/edu/westga/cs6910/pig/model/Game.java
+++ src/edu/westga/cs6910/pig/model/Game.java
@@ -15,7 +15,7 @@ public class Game implements Observable {
 	/**
 	 * The goal score for the game
 	 */
-	public static final int GOAL_SCORE = 200;
+	public static final int GOAL_SCORE = 20;
 
 	private ObjectProperty<Player> currentPlayerObject;
 	private HumanPlayer theHuman;
