1a82707447a57295b06cd8fe509a665124bf9096
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-15 00:25:26 -0400

    Moved test file

--- src/edu/westga/cs6910/pig/tests/DieWhenRoll.java
+++ src/edu/westga/cs6910/pig/tests/DieWhenRoll.java
@@ -1,8 +1,10 @@
-package edu.westga.cs6910.pig.model;
+package edu.westga.cs6910.pig.tests;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.Die;
 
 /**
  * Tests die class roll method. 
