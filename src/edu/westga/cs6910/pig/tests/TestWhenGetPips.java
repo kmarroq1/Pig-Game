2c035c97a1af7c3b6e0d391cf576ac10bd1127e2
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-15 00:12:13 -0400

    Moved test folder

--- src/edu/westga/cs6910/pig/tests/TestWhenGetPips.java
+++ src/edu/westga/cs6910/pig/tests/TestWhenGetPips.java
@@ -1,8 +1,10 @@
-package edu.westga.cs6910.pig.model;
+package edu.westga.cs6910.pig.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.Die;
 
 /**
  * Tests getter method in die class.
