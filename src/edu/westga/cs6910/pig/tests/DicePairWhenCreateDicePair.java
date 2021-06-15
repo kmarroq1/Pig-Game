0aff22d07bf2cb853bb8c4d70681d4a719d86c26
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-15 17:05:55 -0400

    Tested DicePair constructor

---
+++ src/edu/westga/cs6910/pig/tests/DicePairWhenCreateDicePair.java
@@ -0,0 +1,26 @@
+package edu.westga.cs6910.pig.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs6910.pig.model.DicePair;
+
+/**
+ * Tests DicePair constructor by calling toString method.
+ * 
+ * @author Kimberly Marroquin
+ * @version Jun 15, 2021
+ */
+class DicePairWhenCreateDicePair {
+
+	/**
+	 * Test method for DicePair constructor.
+	 */
+	@Test
+	void testToString() {
+		DicePair testDicePair = new DicePair();
+		assertEquals("pips: 1, pips: 1", testDicePair.toString());
+	}
+
+}
