508b88c6d936c8463a36b51c24305ea674a82f85
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-21 23:14:12 -0400

    Modified controller to use default strategy

--- src/edu/westga/cs6910/pig/controllers/Main.java
+++ src/edu/westga/cs6910/pig/controllers/Main.java
@@ -3,6 +3,7 @@ package edu.westga.cs6910.pig.controllers;
 import edu.westga.cs6910.pig.model.ComputerPlayer;
 import edu.westga.cs6910.pig.model.Game;
 import edu.westga.cs6910.pig.model.HumanPlayer;
+import edu.westga.cs6910.pig.model.strategies.CautiousStrategy;
 import edu.westga.cs6910.pig.view.PigPane;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -19,7 +20,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("CS6910: Simple Pig");
-		Game theGame = new Game(new HumanPlayer("Human"), new ComputerPlayer());
+		CautiousStrategy strategy = new CautiousStrategy();
+		Game theGame = new Game(new HumanPlayer("Human"), new ComputerPlayer(strategy));
 
 		PigPane root = new PigPane(theGame);
 		Scene scene = new Scene(root, 700, 220);
