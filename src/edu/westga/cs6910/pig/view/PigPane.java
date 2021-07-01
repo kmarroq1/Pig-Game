c93bb3936269f20251119b71038c2888dbb5ceac
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-30 22:05:03 -0400

    Updated goal setter pane location and functionality

--- src/edu/westga/cs6910/pig/view/PigPane.java
+++ src/edu/westga/cs6910/pig/view/PigPane.java
@@ -7,13 +7,16 @@ import edu.westga.cs6910.pig.model.strategies.GreedyStrategy;
 import edu.westga.cs6910.pig.model.strategies.RandomStrategy;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
@@ -22,6 +25,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
  * Defines a GUI for the Pig game. This class was started by CS6910
@@ -36,6 +40,7 @@ public class PigPane extends BorderPane {
 	private ComputerPane pnComputerPlayer;
 	private StatusPane pnGameInfo;
 	private Pane pnChooseFirstPlayer;
+	private Pane pnGoalSetter;
 	private MenuBarPane menuPane;
 
 	/**
@@ -70,11 +75,11 @@ public class PigPane extends BorderPane {
 	 * 
 	 */
 	private void addFirstPlayerChooserPane() {
-		HBox topBox = new HBox();
-		topBox.getStyleClass().add("pane-border");
+		HBox playerBox = new HBox();
+		playerBox.getStyleClass().add("pane-border");
 		this.pnChooseFirstPlayer = new NewGamePane(this.theGame);
-		topBox.getChildren().add(this.pnChooseFirstPlayer);
-		this.pnContent.setTop(topBox);
+		playerBox.getChildren().add(this.pnChooseFirstPlayer);
+		this.pnContent.setTop(playerBox);
 	}
 
 	/**
@@ -308,6 +313,8 @@ public class PigPane extends BorderPane {
 		private Player theHuman;
 		private Player theComputer;
 		private Player randomPlayer;
+		private TextField goalScore;
+		private Label confirmation;
 
 		private NewGamePane(Game theGame) {
 			this.theGame = theGame;
@@ -338,8 +345,49 @@ public class PigPane extends BorderPane {
 			buttonGroup.getToggles().addAll(this.radHumanPlayer, this.radComputerPlayer, this.radRandomPlayer);
 
 			HBox buttonsBox = new HBox(20, this.radHumanPlayer, this.radComputerPlayer, this.radRandomPlayer);
+			buttonsBox.getStyleClass().add("pane-border");
+			PigPane.this.pnGoalSetter = this.addGoalSetter();
 
-			super.add(buttonsBox, 0, 0);
+			super.add(PigPane.this.pnGoalSetter, 0, 0);
+			super.add(buttonsBox, 1, 0);
+		}
+
+		private VBox addGoalSetter() {
+			Label enterLabel = new Label("Enter goal score:");
+			this.goalScore = new TextField();
+			this.goalScore.setPromptText("20");
+			this.goalScore.setPrefWidth(50);
+			Button goalSetter = new Button("Update");
+			this.confirmation = new Label("");
+			goalSetter.setOnAction(new GoalScoreListener());
+			HBox goalBox = new HBox();
+			goalBox.getChildren().addAll(enterLabel, this.goalScore, goalSetter);
+			goalBox.setSpacing(10);
+			VBox confirmedGoalBox = new VBox();
+			confirmedGoalBox.getChildren().addAll(goalBox, this.confirmation);
+			return confirmedGoalBox;
+		}
+
+		/**
+		 * Defines the listener for the goal setter.
+		 */
+		private class GoalScoreListener implements EventHandler<ActionEvent> {
+			@Override
+			/**
+			 * Enables the Update button to update the goal score.
+			 */
+			public void handle(ActionEvent event) {
+				try {
+					int score = Integer.parseInt(NewGamePane.this.goalScore.getText());
+					PigPane.this.theGame.setGoalScore(score);
+					NewGamePane.this.confirmation
+							.setText("The goal was updated to " + PigPane.this.theGame.getGoalScore() + " points");
+					PigPane.this.pnGameInfo.invalidated(PigPane.this.theGame);
+					PigPane.this.pnGoalSetter.setDisable(true);
+				} catch (IllegalArgumentException exception) {
+					PigPane.this.theGame.setGoalScore(20);
+				}
+			}
 		}
 
 		/**
