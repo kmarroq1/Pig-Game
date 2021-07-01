c55c42888d9641f65b8c3da88f1f2dca905c3076
Kimberly Marroquin <marroquin.kim3@gmail.com>
2021-06-30 22:52:48 -0400

    Updated feature document

--- src/edu/westga/cs6910/pig/view/StatusPane.java
+++ src/edu/westga/cs6910/pig/view/StatusPane.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 
@@ -22,7 +21,6 @@ public class StatusPane extends GridPane implements InvalidationListener {
 	private Game theGame;
 	private Label lblStatus;
 	private Button playAgain;
-	private TextField enterGoal;
 
 	/**
 	 * Creates a new status pane that observes the specified game.
@@ -46,61 +44,27 @@ public class StatusPane extends GridPane implements InvalidationListener {
 		topBox.getChildren().add(new Label("~~ Game Info ~~"));
 		this.add(topBox, 0, 0);
 
-		Label enterLabel = new Label("Enter goal score:");
-		this.enterGoal = new TextField();
-		this.enterGoal.setPrefWidth(50);
-		this.goalScoreHandler(this.enterGoal);
-		HBox topMiddleBox = new HBox();
-		topMiddleBox.getStyleClass().add("box-padding");
-		topMiddleBox.getChildren().addAll(enterLabel, this.enterGoal);
-		topMiddleBox.setSpacing(10);
-		this.add(topMiddleBox, 0, 1);
-
 		HBox middleBox = new HBox();
 		middleBox.getStyleClass().add("box-padding");
 		this.lblStatus = new Label(this.theGame.toString());
 		middleBox.getChildren().add(this.lblStatus);
-		this.add(middleBox, 0, 2);
+		this.add(middleBox, 0, 1);
 
 		HBox buttonBox = new HBox();
 		this.playAgain = new Button("Play Again");
 		this.playAgain.setOnAction(new PlayAgainListener());
 		buttonBox.getChildren().add(this.playAgain);
-		this.add(buttonBox, 0, 3);
-	}
-
-	/**
-	 * Sets up new goal score event handler.
-	 * 
-	 * @param goalScore score input
-	 */
-	private void goalScoreHandler(TextField goalScore) {
-		goalScore.setOnAction(new EventHandler<ActionEvent>() {
-			/**
-			 * Updates goal score. Resets to default if invalid number is entered.
-			 */
-			@Override
-			public void handle(ActionEvent arg0) {
-				try {
-					int score = Integer.parseInt(goalScore.getText());
-					StatusPane.this.theGame.setGoalScore(score);
-				} catch (IllegalArgumentException exception) {
-					StatusPane.this.theGame.setGoalScore(20);
-				}
-			}
-		});
+		this.add(buttonBox, 0, 2);
 	}
 
 	@Override
 	public void invalidated(Observable observable) {
 		this.lblStatus.setText(this.theGame.toString());
-
+		
 		if (this.theGame.isGameOver()) {
 			this.playAgain.setDisable(false);
-			this.enterGoal.setDisable(false);
 		} else {
 			this.playAgain.setDisable(true);
-			this.enterGoal.setDisable(true);
 		}
 	}
 
